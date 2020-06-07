7b93a992f8e5fcfb2277701bee00aa3f5f1a80b3
Winnie Ding <winnie@ironox.com>
2020-06-07 15:41:08 -0700

    Test commit

---
+++ Game24Solver.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# ============================================================================= 
+#  Name:            Game24Solver
+#  Purpose:         Determine whether a game of 24 is solveable.
+#   
+#  Author:          Winnie
+#  Date Created:    Sun Jun  7 15:19:29 2020
+# =============================================================================
+
+
+
+def tester(a, b, c, d):
+     return a + b + c + d
+
+
+if __name__ == "__main__": 
+     print(tester(1 , 2, 1, 2))
+     
