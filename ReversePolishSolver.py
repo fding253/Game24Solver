5634057c9bdc407524c0a11e929752faa210e2ea
Fiona Ding <fding253@gmail.com>
2020-10-31 17:47:23 -0700

    Create stack object

--- ReversePolishSolver.py
+++ ReversePolishSolver.py
@@ -6,6 +6,54 @@
 #  Date Created:    10/29/2020
 # =============================================================================
 
+class Stack():
+    """
+    Stack container
+    """
+    def __init__(self):
+        self.stack = list()
+
+        # Count of elements in stack
+        self.count = 0
+
+        # Index of top of stack
+        self.top = -1
+
+    def push(self, value):
+        """
+        Push a value onto stack.
+        """
+        # Move top tracker
+        self.top += 1
+
+        # Add new value to stack
+        self.stack.append(value)
+
+        # Increment count
+        self.count += 1
+
+    def pop(self):
+        """
+        Remove and return value at top of stack.
+        """
+        if self.count > 0:
+            # Store value at top of stack
+            output = self.stack[self.top]
+
+            # Remove top value from stack
+            del self.stack[self.top]
+
+            # Move top tracker
+            self.top -= 1
+
+            # Decrement count
+            self.count -= 1
+
+            return output
+
+        return None
+
+
 def ReversePolishSolver(combinations_list):
     """
     Solves a given problem in reverse polish notation
